4cddaf03ba92aa1400bb4fee68ac0da1635f581e
Mahek-Dave <mahek@shvasa.com>
2024-05-07 00:22:25 +0530

    _

--- onboarding/contoller.js
+++ onboarding/contoller.js
@@ -1,7 +1,8 @@
 import model from "./model.js";
-console.log("controller works");
-window.addEventListener("load", function () {
+
+window.addEventListener("click", function () {
   const one = async function () {
+    console.log("controller works");
     const test = await model.loadFreeTrialData(prod_MjTaCamDna2hFQ);
     console.log(test);
   };
