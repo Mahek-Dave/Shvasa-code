1f5c99b965b23e556fe8d8a5377db7a14eedb11c
Mahek-Dave <mahek@shvasa.com>
2024-05-07 13:31:31 +0530

    click event added

--- onboarding/contoller.js
+++ onboarding/contoller.js
@@ -1,6 +1,6 @@
 import * as model from "./model.js";
 
-window.addEventListener("load", function () {
+window.addEventListener("click", function () {
   const callFn = async function () {
     console.log("controller works");
     const test = await model.loadPackageData("6573f84acfed902b36249750");
