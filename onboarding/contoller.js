2f9031b18384485992eab2dd1f4b5f7cb0c91460
Mahek-Dave <mahek@shvasa.com>
2024-05-07 13:33:48 +0530

    console added

--- onboarding/contoller.js
+++ onboarding/contoller.js
@@ -1,6 +1,9 @@
 import * as model from "./model.js";
 
+console.log(model);
+
 window.addEventListener("click", function () {
+  console.log("testing");
   const callFn = async function () {
     console.log("controller works");
     const test = await model.loadPackageData("6573f84acfed902b36249750");
