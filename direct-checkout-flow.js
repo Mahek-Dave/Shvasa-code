77d3b90a9a3ec59892d878248d8703ea9601fb48
Mahek-Dave <90769005+Mahek-Dave@users.noreply.github.com>
2024-05-30 12:12:23 +0530

    Create direct-checkout-flow.js

---
+++ direct-checkout-flow.js
@@ -0,0 +1,67 @@
+// IIFE function
+(() => {
+  
+  let iframeAdded = false;
+  let popup;
+
+  // Get url to fetch UTM paras
+  const pageUrl = encodeURIComponent(window.location.href);
+
+  // Signup iframe
+  const iframeHTML = `<iframe class="signup-popup" src="https://app.shvasa.com/widgets?widget=loginPopup&url=${pageUrl}"></iframe>`;
+
+  // Add popup to body
+  const addPopup = (html) =>
+    document.body.insertAdjacentHTML("beforeend", html);
+    
+  // Add popup after page loads
+  window.addEventListener("load", function () {
+    if (iframeAdded === true) return;
+    addPopup(iframeHTML);
+    iframeAdded = true;
+    //console.log(iframeAdded);
+  });
+
+  // Elements
+  const allBtns = [...document.querySelectorAll(".free-trial-wrapper")];
+  //const closeBtn = document.querySelector(".close-signup-popup");
+  const mainBody = document.querySelector(".page-wrapper");
+
+  // Display popup
+  const showPopup = () => {
+    if (iframeAdded === false) {addPopup(iframeHTML)};
+
+    mainBody.style.overflow = "hidden";
+    mainBody.style.height = "100vh";
+    closeBtn.style.display = "flex";
+    popup = document.querySelector(".signup-popup");
+    popup.style.display = "flex";
+    iframeAdded = true;
+   //console.log(iframeAdded);
+  };
+
+  // Hide popup
+  const closePopup = () => {
+    popup.style.display = "none";
+    closeBtn.style.display = "none";
+    mainBody.style.overflow = "auto";
+    mainBody.style.height = "auto";
+  };
+
+  // Events
+  allBtns.forEach((btn) => btn.addEventListener("click", showPopup));
+  closeBtn.addEventListener("click", closePopup);
+
+  window.addEventListener("message", function (event) {
+    console.log(
+      "Message received from the child: " + JSON.stringify(event.data)
+    ); 
+    
+  // Message received from child
+  if (event.data?.event === "loggedIn") {
+    window.location =
+    "https://shvasa-staging-web.vercel.app/onboardingv2?widget=loginpopup&token=" +
+     event.data?.token;
+    }
+  });
+})();
