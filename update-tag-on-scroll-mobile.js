fb73aeae263d9e2135550626f72a0ae2343f06e7
Mahek-Dave <90769005+Mahek-Dave@users.noreply.github.com>
2024-11-18 21:16:35 +0530

    Update update-tag-on-scroll-mobile.js

--- update-tag-on-scroll-mobile.js
+++ update-tag-on-scroll-mobile.js
@@ -1,4 +1,5 @@
-const updateTag = function(){if(window.innerWidth < 990) return;
+const updateTag = function(){
+if(!window.innerWidth < 990) return;
 
 const allBenefitImages = Array.from(document.querySelectorAll('.subscription-benefits-image-wrapper'));
 const tagTitle = document.querySelector('[data-benefit-tag-title]');
