3d3d238e19b66444df9b76c8163392165b0db767
Mahek-Dave <90769005+Mahek-Dave@users.noreply.github.com>
2024-11-18 21:36:41 +0530

    Update update-tag-on-scroll-mobile.js

--- update-tag-on-scroll-mobile.js
+++ update-tag-on-scroll-mobile.js
@@ -42,7 +42,7 @@ const callback = (entries, observer) =>{
 
 const options = {
     root : null,
-    rootMargin : "-50%"
+    rootMargin : "-100%"
 }
 
 const observer = new IntersectionObserver(callback, options);
