4866125da70bc784b8b9d6b6e49943448d2845b8
Mahek-Dave <90769005+Mahek-Dave@users.noreply.github.com>
2024-12-20 20:16:25 +0530

    Update getUTMData.js

--- getUTMData.js
+++ getUTMData.js
@@ -1,3 +1,4 @@
+/*
 const getUTMParams = function (marketingField, parentEl) {
   const marketingData = window.location.search;
   const marketingFieldInput = document.querySelector(`${marketingField}`);
@@ -21,4 +22,34 @@ const getUTMParams = function (marketingField, parentEl) {
 
   marketingFieldInput && (marketingFieldInput.value = marketingData.slice(1));
 
+};
+*/
+
+const getUTMParams = function (marketingField, parentEl) {
+  const marketingData = window.location.search;
+  const marketingFieldInputArr = Array.from(
+    document.querySelectorAll(`${marketingField}`)
+  );
+
+  const params = new URLSearchParams(marketingData);
+
+  const utmObj = {};
+
+  const fieldHTML = function (name, value) {
+    return `<input type="hidden" name="${name}" value="${value}">`;
+  };
+
+  const parentElement = Array.from(document.querySelectorAll(`${parentEl}`));
+
+  params.forEach((value, key) => {
+    utmObj[key] = value;
+    const marketingFieldHTML = fieldHTML(key, value);
+
+    parentElement.forEach((el) =>
+      el.insertAdjacentHTML("beforeend", marketingFieldHTML)
+    );
+  });
+
+  marketingFieldInputArr > 0 &&
+    (marketingFieldInputArr.value = marketingData.slice(1));
 };
