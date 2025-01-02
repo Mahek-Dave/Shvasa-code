1f2cf20d7cb3624f27e5a651deb74ea0beeabcc4
Mahek-Dave <90769005+Mahek-Dave@users.noreply.github.com>
2025-01-02 13:08:37 +0530

    Update set-current-time.js

--- set-current-time.js
+++ set-current-time.js
@@ -2,7 +2,7 @@ const setCurrentTimeToTarget = function (targetEl) {
   const target = Array.from(
     document.querySelectorAll(`${targetEl}`)
   );
-  const currentTime = new Date().toLocaleTimeString("en-US").slice(0, -6);
+  const currentTime = new Date().toLocaleTimeString("en-US",{hour : '2-digit', minute: '2-digit'});
 
   target.forEach((text) => (text.innerHTML = currentTime));
 };
