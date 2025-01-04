d8278bac266fa5fdf9acf14a74e188aa1fbc3b0d
Mahek-Dave <90769005+Mahek-Dave@users.noreply.github.com>
2025-01-04 14:27:18 +0530

    Update render-review-slider.js

--- render-review-slider.js
+++ render-review-slider.js
@@ -104,10 +104,7 @@ const renderReviewSlides = async () => {
   for (const [i, rev] of reviewObject.reviewsArr.entries()) {
     const html = await generateMarkup(rev, i);
     reviewObject.reviewParentEl.insertAdjacentHTML("beforeend", html);
-  }
-
-  window.addEventListener("load", () => {
     window.Webflow.require("slider").redraw();
-  });
+  }
 };
 renderReviewSlides();
