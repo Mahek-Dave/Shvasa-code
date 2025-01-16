ab1b59fa194275c5a87fa847ca78b969e7255c2d
Mahek-Dave <90769005+Mahek-Dave@users.noreply.github.com>
2025-01-16 14:35:06 +0530

    Update add-iframe-popup.js

--- add-iframe-popup.js
+++ add-iframe-popup.js
@@ -18,7 +18,7 @@ const addIframePopup = function (targetEl=`[data-flow="free-trial"]`, popupRequi
   <div class="iframe-popup-container">
     <div class="iframe-popup-wrapper">
       <div class="iframe-popup w-embed w-iframe">
-        <iframe ${allowCameraMic ? 'allow="camera *;microphone *; display-capture *"' : '' } class="iframe-popup" src="${iframeURL}&${pageUrl}"></iframe>
+        <iframe ${allowCameraMic ? 'allow="camera *;microphone *; display-capture *"' : '' } class="iframe-popup" src="${iframeURL}&url=${pageUrl}"></iframe>
       </div>
       <div class="iframe-popup-close-btn-wrapper w-embed">
         <svg class="iframe-popup-close-btn" width="100%" height="100%" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
