32bb80af70a29432c3e994e84c4919718e6488ff
Mahek-Dave <90769005+Mahek-Dave@users.noreply.github.com>
2025-04-01 12:47:14 +0530

    Update add-iframe-popup.js

--- add-iframe-popup.js
+++ add-iframe-popup.js
@@ -1,4 +1,4 @@
-const addIframePopup = function (targetEl=`[data-flow="free-trial"]`, popupRequired=true, iframeURL="https://app.shvasa.com/widgets?widget=loginPopup-v2", redirectURL="https://app.shvasa.com/dashboard?widget=loginpopup", allowCameraMic=false, packageId=undefined) {
+const addIframePopup = function (targetEl=`[data-flow="free-trial"]`, popupRequired=true, iframeURL="https://app.shvasa.com/widgets?widget=loginPopup-v2", redirectURL="https://app.shvasa.com/dashboard?widget=loginpopup", allowCameraMic=false, packageId=undefined, signLabel="start%20a%207%20day%20free%20trial") {
   
   // Elements
   const allBtns = [...document.querySelectorAll(targetEl)];
@@ -13,7 +13,7 @@ const addIframePopup = function (targetEl=`[data-flow="free-trial"]`, popupRequi
   // Get url to fetch UTM paras
   const pageUrl = encodeURIComponent(window.location.href);
 
-  // ProductID
+  // ProductID Check
   const checkProductID = packageId ? `&packageId=${packageId}` : "";
   console.log(checkProductID, packageId )
 
@@ -22,7 +22,7 @@ const addIframePopup = function (targetEl=`[data-flow="free-trial"]`, popupRequi
   <div class="iframe-popup-container">
     <div class="iframe-popup-wrapper">
       <div class="iframe-popup w-embed w-iframe">
-        <iframe ${allowCameraMic ? 'allow="camera *;microphone *; display-capture *"' : '' } class="iframe-popup" src="${iframeURL}&url=${pageUrl}${checkProductID}"></iframe>
+        <iframe ${allowCameraMic ? 'allow="camera *;microphone *; display-capture *"' : '' } class="iframe-popup" src="${iframeURL}&url=${pageUrl}&signLabel=${signLabel}${checkProductID}"></iframe>
       </div>
       <div class="iframe-popup-close-btn-wrapper w-embed">
         <svg class="iframe-popup-close-btn" width="100%" height="100%" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
