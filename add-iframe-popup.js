b478e68f3985c2508ef3940dea7df2486ea81e81
Mahek-Dave <90769005+Mahek-Dave@users.noreply.github.com>
2025-04-02 10:40:45 +0530

    Update add-iframe-popup.js

--- add-iframe-popup.js
+++ add-iframe-popup.js
@@ -144,7 +144,7 @@ const addIframePopup = function ({
   </div>
   `;
 
-  Add popup to body
+  // Add popup to body
   const addPopup = (html) => {
     document.body.insertAdjacentHTML("beforeend", html);
     closeBtn = document.querySelector(".iframe-popup-close-btn");
