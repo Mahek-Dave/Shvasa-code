04b0fadad087aebbaeb4e870ba7df633e4d15c57
Sahil <sahil@shvasa.com>
2026-03-21 21:24:23 +0530

    Update add-teachers-data.js

--- add-teachers-data.js
+++ add-teachers-data.js
@@ -49,7 +49,7 @@ const addTeachersData = async function () {
   // Generate HTML
   const generateHTML = function (data, i, arr) {
     const fullText = data?.classFeedback?.text || "";
-    const maxLength = 180;
+    const maxLength = 100;
 
     const isLong = fullText.length > maxLength;
     const shortText = isLong
